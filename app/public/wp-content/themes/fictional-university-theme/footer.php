45e5059537dd47efca0331354702a9dc3c6a8a9c
Javi Villarreal <javivillarreal@Javis-MacBook-Pro-2.local>
2025-04-22 10:30:17 -0500

    added dynamic menus

--- app/public/wp-content/themes/fictional-university-theme/footer.php
+++ app/public/wp-content/themes/fictional-university-theme/footer.php
@@ -11,24 +11,29 @@
           <div class="site-footer__col-two-three-group">
             <div class="site-footer__col-two">
               <h3 class="headline headline--small">Explore</h3>
-              <nav class="nav-list">
-                <ul>
-                  <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                <nav class="nav-list">
+                    <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'FooterLocationOne'
+                        ));
+                    ?>
+                <!-- <ul>
+                  <li><a href="">About Us</a></li>
                   <li><a href="#">Programs</a></li>
                   <li><a href="#">Events</a></li>
                   <li><a href="#">Campuses</a></li>
-                </ul>
-              </nav>
+                </ul> -->
+                </nav>
             </div>
 
             <div class="site-footer__col-three">
               <h3 class="headline headline--small">Learn</h3>
               <nav class="nav-list">
-                <ul>
-                  <li><a href="#">Legal</a></li>
-                  <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
-                  <li><a href="#">Careers</a></li>
-                </ul>
+                <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'FooterLocationTwo'
+                    ));
+                ?>
               </nav>
             </div>
           </div>
