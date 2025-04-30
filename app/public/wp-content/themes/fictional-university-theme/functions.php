980563e44088d5e1a218c79468090fef215d8500
Javi Villarreal <javivillarreal@javis-mbp-2.lan>
2025-04-29 19:55:27 -0500

    clean up code

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,28 @@
 <?php
 
+function pageBanner($args = NULL) {
+    if (!$args['title']) {
+       $args['title'] = get_the_title();
+    }
+    if (!$args['subtitle']) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
+    ?>
+     <div class="page-banner">
+            <div class="page-banner__bg-image" 
+                 style="background-image: url(
+                    <?php $pageBannerImage = get_field('page_banner_background_image'); 
+                      echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
+                <div class="page-banner__content container container--narrow">
+                    <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+                    <div class="page-banner__intro">
+                        <p><?php echo $args['subtitle']; ?></p>
+                    </div>
+                </div>
+            </div>
+<?php }
+
 function university_files() {
     wp_enqueue_script('main-university_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
